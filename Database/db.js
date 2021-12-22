2744498dd4fd65efba1fa8791bf043e489973237
Roshan Oscar Sah <46841614+RoshanOscarSah@users.noreply.github.com>
2021-12-22 17:01:06 +0545

    Update db.js

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://user:<password>@oscar.wpgq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://user:<password123@%>@oscar.wpgq9.mongodb.net/oscar?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology : true
