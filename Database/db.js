397a484f728f9607e7a3139756e607dbb3c4bf2c
Roshan Oscar Sah <46841614+RoshanOscarSah@users.noreply.github.com>
2021-12-22 16:56:22 +0545

    Update db.js

--- Database/db.js
+++ Database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://127.0.0.1:27017/cylinder',{
+mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://user:<password>@oscar.wpgq9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology : true
